f436a969217e323c42a83ae124aec4bae7ebd4c8
damar-git <davide.rapacciuolo@gmail.com>
2020-05-09 22:28:08 +0200

    [refactoring] updating SwaggerConfig DocumentationType -> SPRING_WEB [/refactoring]

--- spring-aop-data/src/main/java/com/damar/aopdata/configuration/SwaggerConfig.java
+++ spring-aop-data/src/main/java/com/damar/aopdata/configuration/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SPRING_WEB)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
